ec999fcdde7442a057682156a25eafea5b798556
Vivek Verma <vivek.verma@mail.mcgill.ca>
2021-04-15 14:49:19 -0400

    apr15

--- script1.R
+++ script1.R
@@ -14,16 +14,93 @@ df <- within(df, {
 })
 
 df$group <- relevel(df$group, ref = "WHITE")
-
+# remove relatives:
+df <- df[-c(918,1951),]
+df$time_interval <- ifelse(is.na(df$time_interval_years_v1),
+                           df$time_interval_years_v2,
+                           df$time_interval_years_v1)
 library(questionr)
 library(interactions)
 library(jtools)
-library(mediation)
-# model 6
+
+# model 1-6
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0
+               + AGE + SEX + group + time_interval,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ PARACETAMOL
+               + AGE + SEX + group + time_interval,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ OPIOIDS
+               + AGE + SEX + group + time_interval,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ ANTIDEPRESSANTS
+               + AGE + SEX + group + time_interval,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~  gabapentin_pregabalin
+               + AGE + SEX + group + time_interval,
+               family=binomial(logit), data=df))
+
 odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
                    PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
-                   gabapentin_pregabalin + AGE + SEX + group + neutro_p_v1,
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval,
                family=binomial(logit), data=df))
+
+# model 7
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval
+               + neutro_p_v1,
+               family=binomial(logit), data=df))
+
+# supplementary 1-4
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval
+               + baso_p_v1,
+               family=binomial(logit), data=df))
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval
+               + eosino_p_v1,
+               family=binomial(logit), data=df))
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval
+               + lympho_p_v1,
+               family=binomial(logit), data=df))
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group + time_interval
+               + mono_p_v1,
+               family=binomial(logit), data=df))
+
+# sup 5-8
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   AGE + SEX + group + time_interval
+               + neutro_p_v1,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   AGE + SEX + group + time_interval
+               + lympho_p_v1,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval
+               + neutro_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval
+               + lympho_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+
 
 summ(glm(Backpn_3m_v1_v2 ~ NSAID_v0 + neutro_p_v1 + AGE + SEX + group,
                family=binomial(logit), data=df))
@@ -38,7 +115,7 @@ odds.ratio(glm(Backpn_3m_v1_v2 ~ ANTIDEPRESSANTS +
                family=binomial(logit), data=df))
 
 # get wbc data
-wbc <- readRDS("./data/UKB_WBC_all_raw.RDS")
+wbc <- readRDS("./data/WBC.RDS")
 df <- merge(df,wbc,by="ID",all.x = T)
 
 library(Rcmdr)
@@ -49,17 +126,16 @@ odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
                family=binomial(logit), data=df))
 
 
-df <- readRDS("./data/df.RDS")
-round(odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
-                         PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
-                         gabapentin_pregabalin + AGE + SEX + group
-                     #+ baso_p_v1
-                     #+ eosino_p_v1
-                     #+ lympho_p_v1
-                     + mono_p_v1
-                     ,family=binomial(logit), data=df)),2)
 
-
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group
+               #+ baso_p_v1
+               #+ eosino_p_v1
+               #+ neutro_p_v1
+               #+ lympho_p_v1
+               #+ mono_p_v1
+               ,family=binomial(logit), data=df))
 
 
 odds.ratio(glm(Backpn_3m_v1_v2 ~ AGE + SEX + group + WBC_v1
@@ -76,7 +152,7 @@ odds.ratio(glm(Backpn_3m_v1_v2 ~ AGE + SEX + group + NSAID_v0
                ,family=binomial(logit), data=df))
 
 
-fit <- glm(Backpn_3m_v1_v2 ~ AGE + SEX + group + NSAID_v0+lympho_p_v1
+fit <- glm(Backpn_3m_v1_v2 ~ AGE.x + SEX + group + NSAID_v0*neutro_p_v1
            ,family=binomial(logit), data=df)
 summary(fit)
 round(odds.ratio(fit),2)
@@ -111,3 +187,148 @@ sens.out <- medsens(med.out, rho.by = 0.1, effect.type = "indirect",
                     sims = 100)
 plot(sens.out, sens.par = "rho", main = "neutrophil %", ylim = c(-0.1, 0.1),
      xlim = c(-1,1))
+saveRDS(df,"df.RDS")
+
+# On 14 APR 2021
+df1 <- read.delim("C:/Users/Vivek/Desktop/UKB_LBP_neutro/depression_anxiety.tsv")
+df1 <- df1[,c(1,2,5)]
+names(df1)[1] <- "ID"
+df <- merge(df,df1,by="ID",all.x = T)
+df$psych_distress1 <- df$depression_v0 + df$anxiety_panic_v0
+df$psych_distress  <- ifelse(df$psych_distress1 > 0, 1, 0)
+df$psych_distress1 <- NULL
+df$psych_distress <- as.factor(df$psych_distress)
+saveRDS(df,"df.RDS")
+
+
+# models
+round(odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                         PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                         gabapentin_pregabalin
+               + AGE + SEX + group + time_interval + psych_distress + mono_p_v1,
+               family=binomial(logit), data=df)),2)
+
+# sup 5-8
+round(odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval+ psych_distress
+               + neutro_p_v1:NSAID_v0,
+               family=binomial(logit), data=df)),2)
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   AGE + SEX + group + time_interval
+               + lympho_p_v1,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval
+               + neutro_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval
+               + lympho_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+
+
+summ(glm(Backpn_3m_v1_v2 ~ NSAID_v0 + neutro_p_v1 + AGE + SEX + group,
+         family=binomial(logit), data=df))
+
+# model 5
+odds.ratio(glm(Backpn_3m_v1_v2 ~ gabapentin_pregabalin + AGE + SEX + group,
+               family=binomial(logit), data=df))
+
+# model 4
+odds.ratio(glm(Backpn_3m_v1_v2 ~ ANTIDEPRESSANTS +
+                   + AGE + SEX + group,
+               family=binomial(logit), data=df))
+
+
+
+l <- c(6:10,17)
+for (i in l) {
+    print(names(df)[i])
+    print(table(df[,64]))
+}
+
+
+# APR 15
+df <- readRDS("C:/Users/vverma3/Desktop/Repos/UKB_LBP_neutro/data/df.RDS")
+UKB_pain <- readRDS("./data/UKB_pain.RDS")
+
+df <- merge(df,UKB_pain,by="ID",all.x = T)
+rm(UKB_pain)
+saveRDS(df,"./data/df.RDS")
+
+
+library(questionr)
+# models 1 -5
+drugs <- c(6,8,10,9,17)
+for (i in drugs) {
+    print(odds.ratio(glm(as.formula(paste0("Backpn_3m_v1_v2"," ~ ",names(df)[i],
+                                     "+AGE+SEX+group+time_interval+psych_distress+pain_count_1m")),
+                   family=binomial(logit), data=df)))
+}
+
+# model 6
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group +
+                   time_interval + psych_distress + pain_count_1m,
+               family=binomial(logit), data=df))
+
+# model 7
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   PARACETAMOL + OPIOIDS + ANTIDEPRESSANTS +
+                   gabapentin_pregabalin + AGE + SEX + group +
+                   time_interval + psych_distress + pain_count_1m +
+                   neutro_p_v1,
+               family=binomial(logit), data=df))
+
+# sup models 1 -4
+cells <- c(50,47,38,41)
+
+for (i in cells) {
+    print(odds.ratio(glm(as.formula(paste0("Backpn_3m_v1_v2"," ~ ",
+    "NSAID_v0+PARACETAMOL+OPIOIDS+ANTIDEPRESSANTS+gabapentin_pregabalin",
+    "+AGE+SEX+group+time_interval+psych_distress+pain_count_1m+",
+                                           names(df)[i])),
+                         family=binomial(logit), data=df)))
+}
+
+
+# sup 5-6
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                         AGE + SEX + group + time_interval+ psych_distress
+                     + pain_count_1m + neutro_p_v1,
+                     family=binomial(logit), data=df))
+odds.ratio(glm(Backpn_3m_v1_v2 ~ NSAID_v0 +
+                   AGE + SEX + group + time_interval+ psych_distress
+               + pain_count_1m + lympho_p_v1,
+               family=binomial(logit), data=df))
+
+
+# sup 7-8
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval+ psych_distress
+               + pain_count_1m + neutro_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+odds.ratio(glm(Backpn_3m_v1_v2 ~
+                   AGE + SEX + group + time_interval+ psych_distress
+               + pain_count_1m + lympho_p_v1:NSAID_v0,
+               family=binomial(logit), data=df))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
